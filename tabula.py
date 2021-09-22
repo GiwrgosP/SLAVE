02a130de021e06c3e433a9167aa0d823f69c780e
GiwrgosP <gparaskevopoulos@amcstudent.edu.gr>
2021-09-22 10:52:05 +0300

    das

--- tabula.py
+++ tabula.py
@@ -2,6 +2,7 @@ import fitz
 
 
 doc = fitz.open('C:\\Users\\Vostro\\Documents\\GitHub\\SLAVE\\Report1.pdf')
-pix = doc.get_pixmap()
-print(pix)
-pix.save("page-%i.png" % page.number)
+for page in doc:
+    pix = page.get_pixmap()
+    print(pix)
+    pix.save("page-%i.png" % page.number)
