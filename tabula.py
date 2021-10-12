abdd60523b757b085355fcb741f5a0c45bdc8c69
GiwrgosP <gparaskevopoulos@amcstudent.edu.gr>
2021-10-12 20:43:11 +0300

    images resize

--- tabula.py
+++ tabula.py
@@ -1,5 +1,11 @@
+from PIL import Image
 import glob
 
-
 images = glob.glob('C:\\Users\\Vostro\\Documents\\GitHub\\SLAVE\\Fotos\\'+"/*.bmp")
-print(images)
+
+for img in images:
+    temp = Image.open(img)
+    str = img[:-4]
+    print(str)
+    resizeImage = temp.resize((430,404))
+    resizeImage.save(str+"resized.bmp")
